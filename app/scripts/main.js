b1eec5a27539aa49467552636f2ce8d52cdba91e
Katherine Wakefield <awakekat@aol.com>
2014-02-01 15:09:21 -0500

    submit function working

---
+++ app/scripts/main.js
@@ -0,0 +1,10 @@
+// Testing Code
+
+var fightSubmit = document.getElementById('fighterForm').addEventListener('submit', fnWrestler)
+
+function fnWrestler(event) {
+	event.preventDefault();
+	console.log('HelloWorld');
+
+}
+
