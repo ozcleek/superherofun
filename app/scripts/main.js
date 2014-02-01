a677fcc654cd39df24b01ded5793c93023629de4
Daniel Donaldson <donaldson.daniel@gmail.com>
2014-02-01 16:09:51 -0500

    Initial code for building Wrestler Prototype and functions. Added click event.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,33 @@
+// FighterData2 Array
+
+var fighterData2 = [ ]
+
 // Testing Code
+function Wrestler (firstname, lastname, fightinWords) {
+if (firstname) {this.wrestlerfname = firstname}
+if (lastname)  {this.wrestlerlname = lastname}
+if (fightinWords) {this.wrestlerwords = fightinWords}
+};
 
-var fightSubmit = document.getElementById('fighterForm').addEventListener('submit', fnWrestler)
+// prototype for new Fighters
+Wrestler.prototype = {
+	firstname: '' || 'Wrestler',
+	lastname: '' || 'Dude',
+	fightinWords: '' || "I hate making quotes."
+};
 
-function fnWrestler(event) {
+// calling info for generating the new Fighter on submit
+$('.submit').click(function(){
+	console.log("Booya");
+	var firstname = $('#firstNameInput').val();
+	var lastname = $('#lastNameInput').val();
+	var fightinWords = $('#fightinWords').val();
+
+	var wrestlerprofile = new Wrestler (firstname,lastname, fightinWords);
+
+	
+	fighterData2.push(wrestlerprofile);
+
 	event.preventDefault();
-	console.log('HelloWorld');
-
-}
+});
 
