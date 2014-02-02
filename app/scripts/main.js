913cc90e41efd0085a4b1b7e4d0243c6d170371a
Daniel Donaldson <donaldson.daniel@gmail.com>
2014-02-01 21:37:24 -0500

    Added javascript to append user input to the bottom of current user data.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,6 +2,15 @@
 
 var fighterData2 = [ ]
 
+// Fighter Avatars
+var fighterAvatars = [
+	"url(images/Mask1c.jpg)",
+	"url(images/Mask2c.jpg)",
+	"url(images/Mask3c.jpg)",
+	"url(images/Mask4c.jpg)",
+	"url(images/Mask5c.jpg)"]
+
+
 // Testing Code
 function Wrestler (firstname, lastname, fightinWords) {
 if (firstname) {this.wrestlerfname = firstname}
@@ -29,5 +38,27 @@ $('.submit').click(function(){
 	fighterData2.push(wrestlerprofile);
 
 	event.preventDefault();
+
+	var coolerStuff	= function(obj){
+		$(".right-column").append('<div class = "panel-heading">'+obj.wrestlerfname+ ' ' +obj.wrestlerlname+'</div>')
+		$(".right-column").append('<blockquote><p>'+obj.wrestlerwords+'</p></blockquote>')
+	}
+
+	function coolStuff(){
+	_.each(fighterData2, function(obj){
+			$(".right-column").prepend('<div class = "panel-heading">'+obj.wrestlerfname+'</div>')
+			// console.log(obj.title)
+		})
+}
+
+
+	// Trying to isolate the newest instance of a wrestlerprofile
+
+
+// coolStuff();
+
+coolerStuff(wrestlerprofile);
+
 });
+
 
