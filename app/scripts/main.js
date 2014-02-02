8645bcef245a7528be2d658e102aeefa24b59298
Katherine Wakefield <awakekat@aol.com>
2014-02-02 13:53:06 -0500

    no significant changes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -26,47 +26,14 @@ $('.submit').click(function(){
 
 	var wrestlerprofile = new Wrestler (firstname,lastname, fightinWords);
 
-	
-	fighterData2.push(wrestlerprofile);
 
 	event.preventDefault();
+
+    var profiletmpl = _.template ($('#profiletmpl').text());
+
+    $('.right-column').append(profiletmpl({data:wrestlerprofile}));
 });
 
 
 
 
-// Validation on text boxes
-
-  $('form').validate({
-        rules: {
-            firstname: {
-                minlength: 3,
-                maxlength: 20,
-                required: true
-            },
-            lastname: {
-                minlength: 3,
-                maxlength: 20,
-                required: true
-            },
-            fightwords: {
-                minlength: 3,
-                maxlength: 20,
-                required: true
-            }
-        },
-        highlight: function(element) {
-            $(element).closest('.form-group').addClass('has-error');
-        },
-        unhighlight: function(element) {
-            $(element).closest('.form-group').removeClass('has-error');
-        },
-         
-        errorPlacement: function(error, element) {
-            if(element.parent('.input-group').length) {
-                error.insertAfter(element.parent());
-            } else {
-                error.insertAfter(element);
-            }
-        }
-    });
