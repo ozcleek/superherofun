c4512fa18a5c863216cb56b8b6da3562871b30a4
Katherine Wakefield <awakekat@aol.com>
2014-02-02 12:25:40 -0500

    nothing constructive

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -35,64 +35,23 @@ $('.submit').click(function(){
 
 
 
-
-
-
-
-
-function Wrestler (firstname, lastname, fightnw, wyr1, wyr2, wyr3) {
-if (firstname) {this.wrestlerfname = firstname}
-if (lastname)  {this.wrestlerlname = lastname}
-}
-
-$('.submit').click(function() {
-
-	var firstName = $('firstName').val();
-	var lastName = $('lastName').val();
-	var fightinWords = $('fightinWords').val();
-	var wouldYouRather1 = $('wouldYouRather1').val();
-	var wouldYouRather2 = $('wouldYouRather2').val();
-	var wouldYouRather3 = $('wouldYouRather3').val();
-
-	var wrestlerProf = new Wrestler(firstname, lastName, fightinWords, wouldYouRather1, wouldYouRather2, wouldYouRather3);
-} 
-
-
-// -------- extra --------
-
-
-Wrestler.prototype = {
-wrestlerfname: '' || 'Wrestler',
-wrestlerlname: '' || 'Dude',
-
-}
-
-$('.submit').click(function(){
-console.log('New Wrestler Added To The Cage!');
-
-var firstname = $('#firstNameInput').val();
-var lastname = $('#secondNameInput').val();
-
-var wrestlerprofile = new Wrestler (firstname,lastname);
-
-fighterData.push(wrestlerprofile);
-
-});
-
-
-
 // Validation on text boxes
 
- $('form').validate({
+  $('form').validate({
         rules: {
             firstname: {
                 minlength: 3,
-                maxlength: 15,
+                maxlength: 20,
                 required: true
             },
             lastname: {
                 minlength: 3,
-                maxlength: 15,
+                maxlength: 20,
+                required: true
+            },
+            fightwords: {
+                minlength: 3,
+                maxlength: 20,
                 required: true
             }
         },
@@ -102,8 +61,7 @@ fighterData.push(wrestlerprofile);
         unhighlight: function(element) {
             $(element).closest('.form-group').removeClass('has-error');
         },
-        errorElement: 'span',
-        errorClass: 'help-block',
+         
         errorPlacement: function(error, element) {
             if(element.parent('.input-group').length) {
                 error.insertAfter(element.parent());
